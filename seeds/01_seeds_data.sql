688770c74b4137d87a18879a7057a06d5e10ba22
fnakhsan <fn.akhsan@gmail.com>
2025-10-01 12:11:32 +0700

    feat: add more data seeds

--- seeds/01_seeds_data.sql
+++ seeds/01_seeds_data.sql
@@ -23,3 +23,42 @@ VALUES
   ('aaaaaaa5-aaaa-aaaa-aaaa-aaaaaaaaaaa5', '11111111-1111-1111-1111-111111111111',
    'Indo Mini', 'ConvenienceStore', 'https://picsum.photos/200/300?random=5.jpg', -6.2400, 106.8500);
 
+-- Items for Sate Enak (merchant aaaaaaa1-...)
+INSERT INTO items (id, merchant_id, name, product_category, price, image_url)
+VALUES
+  ('bbbbbbb1-1111-1111-1111-bbbbbbbbbbb1', 'aaaaaaa1-aaaa-aaaa-aaaa-aaaaaaaaaaa1',
+   'Sate Ayam', 'Makanan', 25000, 'https://picsum.photos/200/200?random=sate1.jpg'),
+  ('bbbbbbb1-1111-1111-1111-bbbbbbbbbbb2', 'aaaaaaa1-aaaa-aaaa-aaaa-aaaaaaaaaaa1',
+   'Sate Kambing', 'Makanan', 35000, 'https://picsum.photos/200/200?random=sate2.jpg');
+
+-- Items for Bakso Mantap (merchant aaaaaaa2-...)
+INSERT INTO items (id, merchant_id, name, product_category, price, image_url)
+VALUES
+  ('bbbbbbb2-2222-2222-2222-bbbbbbbbbbb1', 'aaaaaaa2-aaaa-aaaa-aaaa-aaaaaaaaaaa2',
+   'Bakso Urat', 'Makanan', 20000, 'https://picsum.photos/200/200?random=bakso1.jpg'),
+  ('bbbbbbb2-2222-2222-2222-bbbbbbbbbbb2', 'aaaaaaa2-aaaa-aaaa-aaaa-aaaaaaaaaaa2',
+   'Bakso Telur', 'Makanan', 25000, 'https://picsum.photos/200/200?random=bakso2.jpg');
+
+-- Items for Ayam Geprek (merchant aaaaaaa3-...)
+INSERT INTO items (id, merchant_id, name, product_category, price, image_url)
+VALUES
+  ('bbbbbbb3-3333-3333-3333-bbbbbbbbbbb1', 'aaaaaaa3-aaaa-aaaa-aaaa-aaaaaaaaaaa3',
+   'Ayam Geprek Original', 'Makanan', 18000, 'https://picsum.photos/200/200?random=geprek1.jpg'),
+  ('bbbbbbb3-3333-3333-3333-bbbbbbbbbbb2', 'aaaaaaa3-aaaa-aaaa-aaaa-aaaaaaaaaaa3',
+   'Ayam Geprek Level 10', 'Makanan', 22000, 'https://picsum.photos/200/200?random=geprek2.jpg');
+
+-- Items for Martabak Boss (merchant aaaaaaa4-...)
+INSERT INTO items (id, merchant_id, name, product_category, price, image_url)
+VALUES
+  ('bbbbbbb4-4444-4444-4444-bbbbbbbbbbb1', 'aaaaaaa4-aaaa-aaaa-aaaa-aaaaaaaaaaa4',
+   'Martabak Manis', 'Makanan', 30000, 'https://picsum.photos/200/200?random=martabak1.jpg'),
+  ('bbbbbbb4-4444-4444-4444-bbbbbbbbbbb2', 'aaaaaaa4-aaaa-aaaa-aaaa-aaaaaaaaaaa4',
+   'Martabak Telur', 'Makanan', 35000, 'https://picsum.photos/200/200?random=martabak2.jpg');
+
+-- Items for Indo Mini (merchant aaaaaaa5-...)
+INSERT INTO items (id, merchant_id, name, product_category, price, image_url)
+VALUES
+  ('bbbbbbb5-5555-5555-5555-bbbbbbbbbbb1', 'aaaaaaa5-aaaa-aaaa-aaaa-aaaaaaaaaaa5',
+   'Air Mineral 600ml', 'Minuman', 5000, 'https://picsum.photos/200/200?random=air.jpg'),
+  ('bbbbbbb5-5555-5555-5555-bbbbbbbbbbb2', 'aaaaaaa5-aaaa-aaaa-aaaa-aaaaaaaaaaa5',
+   'Chiki Balls', 'Snack', 3000, 'https://picsum.photos/200/200?random=chiki.jpg');
