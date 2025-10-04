86bb9df897d8f48bf05aed492a2d51693ede949d
arfiansr <srarfian@gmail.com>
2025-10-04 22:11:55 +0700

    fix : purchase route

--- internal/app/purchase/routes.go
+++ internal/app/purchase/routes.go
@@ -1,18 +1,23 @@
 package purchase
 
 import (
+	"belimang/internal/middleware"
 	"belimang/internal/pkg/jwt"
 
 	"github.com/gin-gonic/gin"
 )
 
 func PurchaseRoutes(router *gin.Engine, handler *PurchaseHandler, jwtService *jwt.JWTService) {
+	nearby := router.Group("/merchants")
+	{
+		nearby.GET("/nearby/:coords", middleware.RequireUser(jwtService), handler.GetMerchantsNearbyHandler)
+	}
 	purchase := router.Group("/users")
-	// purchase.Use(middleware.RequireUser(jwtService))
+	purchase.Use(middleware.RequireUser(jwtService))
+
 	{
 		purchase.POST("/estimate", handler.Estimate)
 		purchase.POST("/orders", handler.CreateOrder)
-		purchase.GET("/merchants/nearby/:coords", handler.GetMerchantsNearbyHandler)
 	}
 
 }
